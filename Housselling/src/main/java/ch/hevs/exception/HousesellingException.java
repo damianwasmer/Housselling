bde04032bfbc6c00745395723ccd568696a0e7e4
Kalja97 <jannis.kalbermatten@students.hevs.ch>
2019-12-18 10:40:15 +0100

    Exception handling, persistence.xml and xhtml files

---
+++ Housselling/src/main/java/ch/hevs/exception/HousesellingException.java
@@ -0,0 +1,21 @@
+package ch.hevs.exception;
+
+public class HousesellingException extends RuntimeException {
+
+	public HousesellingException() {
+		super();
+	}
+
+	public HousesellingException(String arg0) {
+		super(arg0);
+	}
+
+	public HousesellingException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public HousesellingException(Throwable arg0) {
+		super(arg0);
+	}
+
+}
